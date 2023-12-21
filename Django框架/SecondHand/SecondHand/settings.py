000f2d3d867fa1f04bbc46d91572eb3986ffd7a5
AchickenMan <2901258576@qq.com>
2023-12-21 21:05:58 +0800

    数据库
    加了models.py存放数据库的表
    加了数据库端口用户名

--- Django框架/SecondHand/SecondHand/settings.py
+++ Django框架/SecondHand/SecondHand/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
@@ -76,11 +77,17 @@ WSGI_APPLICATION = 'SecondHand.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',		# 要连接的数据库ip
+        'PORT': '3306',				# 数据库连接端口，一般默认为3306
+        'NAME': 'secondhand',			# 已经存在的数据库名称
+        'USER': 'root',				# 用户名
+        'PASSWORD': '123456',				# 用户密码
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
